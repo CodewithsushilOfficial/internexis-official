c6b48aa6b22adfa6a7479be2fccf30e8c132d606
Sushil kumar kushwaha <codewithsushil7236@gmail.com>
2025-07-16 14:32:59 +0530

    i

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -3,8 +3,6 @@ import { HeroSection } from "../components/Home/HeroSection";
 import { About } from "../pages/About/About";
 import { Internship } from "../pages/Internship/Internship";
 import { OurServices } from "../components/Home/OurServices";
-// import { Pricing } from "../components/features/internships/Pricing";
-// import { Certificates } from "../components/features/internships/Certificates";
 import { Testimonials } from "../components/Home/Testimonials";
 import { Contact } from "../pages/Contact/Contact";
 import { Partners } from "../components/Home/Partners";
@@ -15,28 +13,11 @@ import { CookiePolicy } from "../components/features/legal/CookiePolicy";
 import { TermsAndConditions } from "../components/features/legal/TermsAndConditionsNew";
 import { MeetInternexis } from "../pages/About/MeetInternexis";
 import { AllPrograms } from "../pages/Internship/AllPrograms";
-// import InternshipProjects from "../components/features/internships/InternshipProjects";
-// import DomainDetails from "../components/features/internships/DomainDetails";
 import { Ambassador } from "../pages/CampusAmbassador/Ambassador";
-// import InternshipApplication from "../components/features/internships/InternshipApplication";
-// import { AdminDashboard } from "../pages/Admin/AdminDashboard";
-// import SimpleAdminLogin from "../components/features/admin/SimpleAdminLogin";
-// import AdminApp from "../components/admin/AdminApp";
 import { Career } from "../pages/Career/Career";
 import { UserPage } from "../pages/UserPage";
 import { CampusAmbassadorApplication } from "../pages/CampusAmbassador/CampusAmbassadorApplication";
 import { CampusAmbassadorTestPage } from "../pages/CampusAmbassador/CampusAmbassadorTestPage";
-
-// Service pages
-// import DigitalSolutionsPage from "../components/features/services/DigitalSolutionsPage";
-// import HackathonsPage from "../components/features/services/HackathonsPage";
-// import WorkWithUsPage from "../components/features/services/WorkWithUsPage";
-// import {
-//   MentorshipPage,
-//   CareerGuidancePage,
-//   FreelanceProjectsPage,
-//   CareerJobsPage,
-// } from "../components/features/services/ServicePages";
 
 export function AppRoutes() {
   return (
--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,5 +1,3 @@
 // Utils exports
 export * from './exportUtils';
-export * from './generateSampleData';
-export * from './testApiConnection';
 export * from './helpers';
